a5a4eb8b39225422073e963ce0b843d6ebee8e6f
Jonathan Abourbih <jon.abourbih@gmail.com>
2018-12-04 21:20:02 +0000

    Add s3 upload

--- lambda/api/lib/image_downloader.js
+++ lambda/api/lib/image_downloader.js
@@ -1,5 +1,10 @@
 const { URL } = require('url');
 const axios = require('axios');
+const AWS = require('aws-sdk');
+const tmp = require('tmp-promise');
+const fs = require('fs');
+
+const objectStore = new AWS.S3();
 
 const extractImageId = url => url.searchParams.get('id');
 const generateDownloadUrl = id => {
@@ -40,5 +45,30 @@ exports.ImageDownloader = {
       console.error(`Error occurred downloading file from ${url}`, error);
       throw error;
     }
+  },
+
+  async uploadImage(key, stream) {
+    return objectStore
+      .putObject({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Body: stream,
+        Key: `images/${key}.jpg`,
+        ContentType: 'image/jpeg'
+      })
+      .promise();
+  },
+
+  async transferFile(url) {
+    return tmp.withFile(async ({ path }) => {
+      const downloadUrl = generateDownloadUrl(url);
+      const keyName = encodeURIComponent(extractImageId(url));
+
+      await this.downloadFile(downloadUrl, fs.createWriteStream(path));
+      await this.uploadImage(keyName, fs.createReadStream(path));
+
+      return new URL(
+        `https://${S3_BUCKET_NAME}.s3.amazonaws.com/images/${keyName}.jpg`
+      ).toString();
+    });
   }
 };
