2feaa08bc11d319ef23c3e8c2d2c62811e4ff1c4
Jonathan Abourbih <jon.abourbih@gmail.com>
2018-12-04 21:55:34 +0000

    Save full record to s3

--- lambda/api/index.js
+++ lambda/api/index.js
@@ -1,10 +1,24 @@
 const { ImageDownloader } = require('./lib/image_downloader');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
 
 exports.handler = async (event, context) => {
-  event.Records.forEach(record => {
+  const records = event.Records.map(async record => {
     const { body } = record;
     const content = JSON.parse(body);
+
+    content.imageUrl = await ImageDownloader.transferFile(content.href);
+
+    return s3
+      .putObject({
+        Key: `data/${content.row}.json`,
+        ContentType: 'application/json',
+        Bucket: process.env.S3_BUCKET_NAME,
+        Body: JSON.stringify(content)
+      })
+      .promise();
   });
 
-  return {};
+  return await Promise.all(records);
 };
